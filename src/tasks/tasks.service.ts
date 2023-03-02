065f23fa76b56ab585cebc9d63a2612f3f85f67e
mli42 <mli@student.42.fr>
2023-03-02 12:05:49 +0100

    feat: Validation Pipes and Error Handling

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -17,7 +17,7 @@ export class TasksService {
     let tasks = this.getAllTasks();
 
     if (status) {
-      tasks = tasks.filter((task) => task.status != status);
+      tasks = tasks.filter((task) => task.status == status);
     }
     if (search) {
       tasks = tasks.filter(
@@ -29,7 +29,12 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id == id);
+    const found = this.tasks.find((task) => task.id == id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with ID '${id}' not found`);
+    }
+    return found;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -46,7 +51,9 @@ export class TasksService {
   }
 
   deleteTaskById(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id != id);
+    const found = this.getTaskById(id);
+
+    this.tasks = this.tasks.filter((task) => task.id != found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus) {
